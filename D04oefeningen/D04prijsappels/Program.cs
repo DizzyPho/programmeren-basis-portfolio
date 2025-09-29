3e30952b9faaf5c2a92cdf37c3d64c3a666fa3b3
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-09-29 10:07:57 +0200

    Add space to console output to make it look nicer

--- D04oefeningen/D04prijsappels/Program.cs
+++ D04oefeningen/D04prijsappels/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Hoeveel kilo appels wil je kopen?");
+            Console.Write("Hoeveel kilo appels wil je kopen? ");
             int kiloAppels = int.Parse(Console.ReadLine());
 
             double prijsPerKilo = kiloAppels switch {
