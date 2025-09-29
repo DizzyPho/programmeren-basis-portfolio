5d5d1e629e9589e596cb9d0f42795e3b53de4744
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-09-29 11:44:16 +0200

    Ask user to input 3 scores

--- D04oefeningen/D04geslaagd/Program.cs
+++ D04oefeningen/D04geslaagd/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            int score1 = UserInputHulpKlasse.UserInputGetal("Wat is je eerste score op 10? ");
+            int score2 = UserInputHulpKlasse.UserInputGetal("Wat is je tweede score op 10? ");
+            int score3 = UserInputHulpKlasse.UserInputGetal("Wat is je derde score op 10? ");
+
         }
-    }
+       }
 }
