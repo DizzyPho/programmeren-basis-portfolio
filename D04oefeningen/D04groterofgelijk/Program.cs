aafe1b73752ecadd0083551519b5df9ad6b84810
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-09-29 08:45:03 +0200

    Compare to numbers and output which is larger

--- D04oefeningen/D04groterofgelijk/Program.cs
+++ D04oefeningen/D04groterofgelijk/Program.cs
@@ -10,6 +10,18 @@
             Console.Write("Geef een tweede getal: ");
             int getal2 = int.Parse(Console.ReadLine());
 
+            if (getal1 > getal2)
+            {
+                Console.WriteLine("Het eerste getal is groter");
+            }
+            else if (getal1 < getal2)
+            {
+                Console.WriteLine("Het tweede getal is groter");
+            } else
+            {
+                Console.WriteLine("Ze zijn even groot");
+            }
+
         }
     }
 }
