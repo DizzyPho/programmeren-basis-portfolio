a8b02e565d78900e1624f781397b71d244b78776
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-09-29 12:09:19 +0200

    Import UserInputHulpKlasse to project

---
+++ D04oefeningen/D04schrikkeljaar/UserInputHulpKlasse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D04geslaagd
+{
+    internal class UserInputHulpKlasse
+    {
+        public static int UserInputGetal(string vraag)
+        {
+            Console.Write(vraag);
+            int input = int.Parse(Console.ReadLine());
+
+            return input;
+        }
+    }
+}
