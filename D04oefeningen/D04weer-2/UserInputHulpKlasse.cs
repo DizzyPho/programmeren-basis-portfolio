dbd508045a6e338d66ddc9eab81ba6a742332d5c
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-09-29 13:38:24 +0200

    Import UserInputHulpKlasse to project

---
+++ D04oefeningen/D04weer-2/UserInputHulpKlasse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInput
+{
+    internal class UserInputHulpKlasse
+    {
+        public static int UserInputGetal(string vraag)
+        {
+            Console.Write(vraag);
+            int input = int.Parse(Console.ReadLine());
+
+            return input;
+        }
+
+        public static string UserInput(string vraag)
+        {
+            Console.Write(vraag);
+            string input = Console.ReadLine();
+
+            return input;
+        }
+
+        public static bool UserInputJaNee(string vraag)
+        {
+            Console.Write(vraag + "(ja/nee) ");
+            string input = Console.ReadLine().ToLower().Trim();
+            bool antwoordIsJa = (input == "ja");
+
+            return antwoordIsJa;
+        } 
+    }
+}
