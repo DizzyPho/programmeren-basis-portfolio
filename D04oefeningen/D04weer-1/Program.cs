086bc9e11b47d095afe12abf3895456483cffe9b
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-09-29 13:31:46 +0200

    Decide what the weather is based on two boolean values

--- D04oefeningen/D04weer-1/Program.cs
+++ D04oefeningen/D04weer-1/Program.cs
@@ -9,7 +9,23 @@ namespace D04weer_1
             bool zon = UserInputHulpKlasse.UserInputJaNee("Schijnt de zon?");
             bool regen = UserInputHulpKlasse.UserInputJaNee("Regent het?");
 
-
+            string weer;
+            if (zon && regen)
+            {
+                weer = "Er is een regenboog.";
+            }
+            else if (zon && !regen)
+            {
+                weer = "Het is een mooie dag.";
+            }
+            else if (!zon && regen) 
+            {
+                weer = "Het is slecht weer.";       
+            }
+            else
+            {
+                weer = "Het is een saaie dag.";
+            }
         }
     }
 }
