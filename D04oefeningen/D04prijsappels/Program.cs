408291448209fdb430b9bff2ed7cf726b055de7b
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-09-29 09:57:40 +0200

    Decide price per kilo based on how many kilos were bought

--- D04oefeningen/D04prijsappels/Program.cs
+++ D04oefeningen/D04prijsappels/Program.cs
@@ -5,9 +5,16 @@
         static void Main(string[] args)
         {
             Console.Write("Hoeveel kilo appels wil je kopen?");
-            int kiloAppels = int.Parse(Console.ReadLine()); 
+            int kiloAppels = int.Parse(Console.ReadLine());
 
+            double prijsPerKilo = kiloAppels switch {
+                > 20 => 2,
+                > 10 => 2.5,
+                _ => 3
+            };
 
+            double totalePrijs = prijsPerKilo * kiloAppels;
+            Console.WriteLine($"Prijs: {totalePrijs}");
         }
     }
 }
