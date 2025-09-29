eb38c13693bb81285c9be1df4833b6df2d91e732
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-09-29 10:54:41 +0200

    Add project for new exercise

---
+++ D04oefeningen/D04score/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace D04score
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Geef je score op honderd in: ");
+        }
+    }
+}
