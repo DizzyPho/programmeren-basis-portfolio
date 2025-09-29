2e6059978ebc7c66dc906faeb352db7a2fe314dc
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-09-29 11:48:41 +0200

    Decide result based on 3 given scores

--- D04oefeningen/D04geslaagd/Program.cs
+++ D04oefeningen/D04geslaagd/Program.cs
@@ -9,6 +9,19 @@
             int score2 = UserInputHulpKlasse.UserInputGetal("Wat is je tweede score op 10? ");
             int score3 = UserInputHulpKlasse.UserInputGetal("Wat is je derde score op 10? ");
 
+            int totaleScore = score1 + score2 + score3;
+
+            string resultaat;
+            if (score1 > 4 && score2 > 4 && score3 > 4)
+            {
+                resultaat = "Geslaagd!";
+            } else if (totaleScore >= 18)
+            {
+                resultaat = "Geslaagd!";
+            } else
+            {
+                resultaat = "Gefaald.";
+            }
         }
        }
 }
