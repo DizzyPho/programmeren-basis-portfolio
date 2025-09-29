0ea3300293387f93ff238f2e8a06b21597fed860
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-09-29 11:04:09 +0200

    Decide letter grade based on percentage grade and give output

--- D04oefeningen/D04score/Program.cs
+++ D04oefeningen/D04score/Program.cs
@@ -5,8 +5,25 @@
         static void Main(string[] args)
         {
             Console.Write("Geef je score op honderd in: ");
-            int scorePercent = int.Parse(Console.ReadLine());   
+            int scorePercent = int.Parse(Console.ReadLine());
 
+            string scoreLetter = scorePercent switch { 
+                < 22 => "F",
+                < 37 => "E",
+                < 52 => "D",
+                < 67 => "C",
+                < 82 => "B",
+                <= 100 => "A",
+                _ => "ongeldig",
+            };
+
+            if(scoreLetter == "ongeldig")
+            {
+                Console.WriteLine("Dit is geen geldig percentage!");
+            } else
+            {
+                Console.WriteLine($"Jouw score komt overeen met een {scoreLetter}");
+            }
 
         }
     }
