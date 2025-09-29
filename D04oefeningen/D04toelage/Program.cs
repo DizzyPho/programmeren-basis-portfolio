c7f84013617ef94121799398ae38b45f4de487b1
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-09-29 11:13:28 +0200

    Calculate and output money when conditions are met

--- D04oefeningen/D04toelage/Program.cs
+++ D04oefeningen/D04toelage/Program.cs
@@ -4,11 +4,20 @@
     {
         static void Main(string[] args)
         {
+            const double toelagePercent = 0.03;
+
             Console.Write("Hoeveel is je jaarinkomen in euro? ");
             int inkomen = int.Parse(Console.ReadLine());
 
             Console.Write("Hoeveel kinderen zijn er in je gezin?");
             int aantalKinderen = int.Parse(Console.ReadLine());
+
+            if (inkomen <= 20000 && aantalKinderen > 2)
+            {
+                double toelage = inkomen * toelagePercent;
+                Console.WriteLine($"Jouw toelage is {toelage} euro.");
+            }
+
         }
     }
 }
