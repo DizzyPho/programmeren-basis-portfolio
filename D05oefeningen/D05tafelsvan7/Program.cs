b7295cb69f1e656f058c12ceb780929e1a075ace
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-02 08:55:33 +0200

    Write multiples of seven

--- D05oefeningen/D05tafelsvan7/Program.cs
+++ D05oefeningen/D05tafelsvan7/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-               
+            int currentNumber = 1;
+            do {
+                int multiplied = currentNumber * 7;
+
+                Console.WriteLine($"{currentNumber} * 7 = {multiplied}");
+
+                currentNumber++;
+            } while (currentNumber <= 10);
         }
     }
 }
