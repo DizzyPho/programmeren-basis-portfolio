1e87b8939ed5a8305d9a9e13eea455c26b5f29f9
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-02 09:21:27 +0200

    Ask user to guess and compare to random number

--- D05oefeningen/D05getalraden/Program.cs
+++ D05oefeningen/D05getalraden/Program.cs
@@ -1,11 +1,29 @@
-﻿namespace D05getalraden
+﻿using Helper.UserInputHelper;
+
+namespace D05getalraden
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Random rand = new Random();
-            int randomNum = rand.Next(1,101);
+            int randomNum = rand.Next(1, 101);
+
+            int guess = UserInput.IntegerInput("Guess a number: ");
+            string result;
+            if (guess > randomNum)
+            {
+                result = "lager!";
+            }
+            else if (guess < randomNum)
+            {
+                result = "hoger!";
+            }
+            else
+            {
+                result = "Disco!!!";
+            }
+            Console.WriteLine(result);
         }
     }
 }
