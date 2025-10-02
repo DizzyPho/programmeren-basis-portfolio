58157cbd9fa1122c1dca3ed7b8ac6536035c6c17
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-02 09:41:18 +0200

    Create project and start from solution of D05som

---
+++ D05oefeningen/D05grootstegetral/Program.cs
@@ -0,0 +1,28 @@
+﻿using Helper.UserInputHelper;
+
+namespace D05grootstegetral
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+
+            do
+            {
+                int addition = UserInput.IntegerInput("Geef een volgend getal in, of -1 om te stoppen en de som te maken: ");
+
+                if (addition == -1)
+                {
+                    break;
+                }
+                total += addition;
+
+            } while (true);
+
+            Console.WriteLine($"Total sum is: {total}");
+
+        }
+    }
+}
+}
