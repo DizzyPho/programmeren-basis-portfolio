ee6c02469d8d3602ea471673400a6b41bed6a99c
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-02 09:54:02 +0200

    Set up project for new exercise

---
+++ D05oefeningen/D05rechthoek/UserInput.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helper.UserInputHelper
+{
+        internal class UserInput
+        {
+            public static string StringInput(string query)
+            {
+                Console.Write(query);
+                string input = Console.ReadLine();
+
+                return input;
+            }
+
+        public static int IntegerInput(string query)
+        {
+            int input = int.Parse(StringInput(query));
+
+            return input;
+        }
+
+        public static double DoubleInput(string query)
+        {
+            double input = double.Parse(StringInput(query));
+
+            return input;
+        }
+        public static bool AnswerIsYes(string query)
+        {
+
+            string input = StringInput(query + "(yes/ja or no/nee) ");
+            input = input.ToLower().Trim();
+
+            bool answerIsYes = (input == "ja" || input == "yes");
+
+            return answerIsYes;
+        }
+
+    }
+    }
