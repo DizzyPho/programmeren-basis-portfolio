1ed2da31d48ad712c772a1cbcdd82e33d72d3551
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-02 10:45:11 +0200

    Catch non-double user input

--- D05oefeningen/D05double/Program.cs
+++ D05oefeningen/D05double/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace D05double
+﻿using Helper.UserInputHelper;
+
+namespace D05double
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double number;
+            try
+            {
+                number = UserInput.DoubleInput("Geef een double getal in: ");
+                Console.WriteLine($"Dankje voor het getal ({number})");
+            } catch
+            {
+                Console.WriteLine("Afgebroken wegens geen double.");
+            }
         }
     }
 }
