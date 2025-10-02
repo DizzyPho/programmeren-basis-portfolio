e24914ab74688c19c74038b3852132cb896ce1f1
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-02 08:58:02 +0200

    Add code from task assignment

--- D05oefeningen/D05hiephieperhiepst/Program.cs
+++ D05oefeningen/D05hiephieperhiepst/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Hoe oud wordt de jarige : ");
+            int leeftijd = int.Parse(Console.ReadLine());
+
+            int jaar = 1;
+            while (jaar <= leeftijd)
+            {
+                Console.WriteLine("Hiep hiep hiep, hoera!");
+                jaar++;
+            }
         }
     }
 }
