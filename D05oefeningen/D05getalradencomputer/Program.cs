85f3f9002c4f63160187bfe4b2cdb281551841da
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-02 12:12:45 +0200

    Solve exercise

--- D05oefeningen/D05getalradencomputer/Program.cs
+++ D05oefeningen/D05getalradencomputer/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace D05getalradencomputer
+﻿using Helper.UserInputHelper;
+
+namespace D05getalradencomputer
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int iterationNumber = 0;
+            int guess;
+            int min = 0;
+            int max = 100;
+            bool numberGuessed = false;
+            do
+            {
+                guess = (min + max) / 2;
+
+                string higherLower = UserInput.StringInput($"I guess {guess}. Higher, lower? or correct? ");
+
+                switch (higherLower)
+                {
+                    case "higher":
+                        min = guess;
+                        break;
+                    case "lower":
+                        max = guess;
+                        break;
+                    case "correct":
+                        numberGuessed = true;
+                        break;
+                    default:
+                        break;
+                }
+
+                iterationNumber++;
+            } while (!numberGuessed);
+
+            Console.WriteLine($"I guessed it in {iterationNumber} guesses!");
         }
     }
 }
