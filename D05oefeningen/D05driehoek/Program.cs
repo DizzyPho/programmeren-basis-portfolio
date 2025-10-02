5243dc8690df8ede229106f8d4bf4ffbf10fd753
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-02 10:14:43 +0200

    Rework solution to create triangle instead

--- D05oefeningen/D05driehoek/Program.cs
+++ D05oefeningen/D05driehoek/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace D05driehoek
+﻿using Helper.UserInputHelper;
+
+namespace D05driehoek
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int triangleBase = UserInput.IntegerInput("Input base length: ");
+            int height = UserInput.IntegerInput("Input height: ");
+
+            string line = "";
+
+            for (int i = 0; i < triangleBase; i++)
+            {
+                line += "*";
+            }
+
+            for (int i = 0; i < height; i++)
+            {
+                line = line.Substring(0, triangleBase - i);
+                Console.WriteLine(line);
+                
+            }
         }
     }
 }
