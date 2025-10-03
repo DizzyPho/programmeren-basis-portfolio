2b6f27e70e078b21c1988045e50588afa2b2877f
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-03 10:01:05 +0200

    Loop game and stop if user guesses incorrectly

--- D06oefeningen/D05simon/Program.cs
+++ D06oefeningen/D05simon/Program.cs
@@ -7,17 +7,22 @@ namespace D05simon
         static void Main(string[] args)
         {
             string numberToRemember = "";
+            string userGuess;
+            do
+            {
+                Random random = new Random();
+                int randomNumber = random.Next(1, 5);
 
-            Random random = new Random();
-            int randomNumber = random.Next(1, 5);
+                numberToRemember += randomNumber;
 
-            numberToRemember += randomNumber;
+                Console.WriteLine($"You have 3 seconds to remember this number: \n{numberToRemember}");
+                System.Threading.Thread.Sleep(3000);
+                Console.Clear();
 
-            Console.WriteLine($"You have 3 seconds to remember this number: \n{numberToRemember}");
-            System.Threading.Thread.Sleep( 3000 );
-            Console.Clear();
+                userGuess = UserInput.StringInput("Type the number here: ");
+            } while (userGuess == numberToRemember);
 
-            string userGuess = UserInput.StringInput("Type the number here: ");
+            Console.WriteLine($"You lost! The number was: {numberToRemember}");
         }
     }
 }
