6911483717126e9f17053c13f98b4e0a653da677
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-03 09:42:48 +0200

    Loop code infinitely

--- D06oefeningen/D05twister/Program.cs
+++ D06oefeningen/D05twister/Program.cs
@@ -8,16 +8,18 @@
             string[] limbs = ["linkerhand", "rechterhand", "linkervoet", "rechtervoet"];
 
             Random rand = new Random();
+            while (true)
+            {
+                int whichLimb = rand.Next(limbs.Length);
+                int whichColor = rand.Next(colors.Length);
 
-            int whichLimb = rand.Next(limbs.Length);
-            int whichColor = rand.Next(colors.Length);
+                string limb = limbs[whichLimb];
+                string color = colors[whichColor];
 
-            string limb = limbs[whichLimb];
-            string color = colors[whichColor];
+                Console.WriteLine($"{limb} op {color}!");
 
-            Console.WriteLine($"{limb} op {color}!");
-
-            System.Threading.Thread.Sleep(7000);
+                System.Threading.Thread.Sleep(7000);
+            }
         }
     }
 }
