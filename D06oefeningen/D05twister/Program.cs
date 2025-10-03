f50a6546432f806f3e2eb8ac2fd4b9baef6bb5ed
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-03 09:29:11 +0200

    Create project for new exercise

---
+++ D06oefeningen/D05twister/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace D05twister
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
