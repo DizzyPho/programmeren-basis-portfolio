1838a99adfe1e1431bbb040ffb0516259fe071db
dizzypho <184835166+DizzyPho@users.noreply.github.com.>
2025-10-04 12:16:38 +0200

    Ask user to input number until negative number is inputted

--- D06oefeningen/D06somevenoneven/Program.cs
+++ D06oefeningen/D06somevenoneven/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace D06somevenoneven
+﻿using Helper.UserInputHelper;
+
+namespace D06somevenoneven
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int userNumber;
+            do
+            {
+                try
+                {
+                    userNumber = UserInput.IntegerInput("Give a number (negative to quit): ");
+                }
+                catch
+                {
+                    Console.WriteLine("Geef een getal in!");
+                    userNumber = -1;
+                    continue;                 
+                }
+                Console.WriteLine("test");
+            } while (userNumber >= 0);
         }
     }
 }
