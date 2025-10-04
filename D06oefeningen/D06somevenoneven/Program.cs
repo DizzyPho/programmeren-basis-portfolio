913611216924101828263a782d51ab2bce99199f
dizzypho <184835166+DizzyPho@users.noreply.github.com.>
2025-10-04 12:01:33 +0200

    Set up project for new exercise

---
+++ D06oefeningen/D06somevenoneven/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace D06somevenoneven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
