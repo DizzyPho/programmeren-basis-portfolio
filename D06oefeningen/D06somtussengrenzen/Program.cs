050e924202d465ac0bb17cbe3fa25f5d330d4c7d
dizzypho <184835166+DizzyPho@users.noreply.github.com.>
2025-10-04 11:49:11 +0200

    Set up project for new exercise

---
+++ D06oefeningen/D06somtussengrenzen/Program.cs
@@ -0,0 +1,12 @@
+﻿using Helper.UserInputHelper;
+
+namespace D06somtussengrenzen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
