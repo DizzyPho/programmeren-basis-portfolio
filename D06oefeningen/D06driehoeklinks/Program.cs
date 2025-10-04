c06f736cbc76c0b9a445819582c4fb3c01df9b0f
dizzypho <184835166+DizzyPho@users.noreply.github.com.>
2025-10-04 13:05:39 +0200

    Set up project for new exercise

---
+++ D06oefeningen/D06driehoeklinks/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace D06driehoeklinks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
