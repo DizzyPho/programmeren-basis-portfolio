fd6f9be2861577054fc6aa7dccb5ede2f20eaf74
dizzypho <184835166+DizzyPho@users.noreply.github.com.>
2025-10-04 13:03:19 +0200

    Print out all multiples of 5 or 3 between user given bounds

--- D06oefeningen/D06veelvouden/Program.cs
+++ D06oefeningen/D06veelvouden/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace D06veelvouden
+﻿using Helper.UserInputHelper;
+
+namespace D06veelvouden
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int lowerBound = UserInput.IntegerInput("Give a lower bound: ");  //exclusive bound
+            int upperBound = UserInput.IntegerInput("Give an upper bound: ");
+
+            int sum = 0;
+
+            for (int i = lowerBound; i <= upperBound; i++)
+            {
+                bool divisibleBy3 = i % 3 == 0;
+                bool divisibleBy5 = i % 5 == 0;
+
+                if (divisibleBy3 || divisibleBy5)
+                {
+                    sum += i;
+                }
+
+
+                
+            }
+            Console.Write($"Sum is of multiples of five and multiples of three is: {sum}");
         }
     }
 }
