421350ed80be94cacc8c43c8dce11f0bcef5791d
dizzypho <184835166+DizzyPho@users.noreply.github.com.>
2025-10-04 11:59:43 +0200

    Sum numbers between upper and lower bound

--- D06oefeningen/D06somtussengrenzen/Program.cs
+++ D06oefeningen/D06somtussengrenzen/Program.cs
@@ -6,8 +6,20 @@ namespace D06somtussengrenzen
     {
         static void Main(string[] args)
         {
-            int lowerBound = UserInput.IntegerInput("Give a lower bound: ");
+            int lowerBound = UserInput.IntegerInput("Give a lower bound: ") + 1;  //exclusive bound
             int upperBound = UserInput.IntegerInput("Give an upper bound: ");
+
+            int sum = 0;
+
+            for (int i = lowerBound; i < upperBound; i++)
+            {
+                string sumString = i == upperBound - 1 ? $"{i} " : $"{i} + ";
+
+                Console.Write(sumString);
+                sum += i;
+            }
+            
+            Console.Write($"= {sum}");
         }
     }
 }
