7dc4ff12e80dd630a1aec04463e6165207172a2a
dizzypho <184835166+DizzyPho@users.noreply.github.com.>
2025-10-04 11:37:43 +0200

    Add project for new exercise

---
+++ D06oefeningen/D06getallentussen20en30/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace D06getallentussen20en30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
