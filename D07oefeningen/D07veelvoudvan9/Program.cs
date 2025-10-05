502a89db2e14f077613019ad608a84d12ebc9873
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-05 21:47:13 +0200

    Change which multiples are displayed according to assigment

--- D07oefeningen/D07veelvoudvan9/Program.cs
+++ D07oefeningen/D07veelvoudvan9/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i <= 10; i++)
+            for (int i = 1; i <= 15; i+=2)
             {
                 int multiple = i * 9;
                 string formatted = $"{i,2:d0} x 9 = {multiple,3:d0}";
