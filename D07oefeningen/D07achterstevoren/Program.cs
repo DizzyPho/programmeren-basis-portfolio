576260f9e2941808cd23274fde650e148bd9c870
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-05 22:17:42 +0200

    Set up project for new exercise

---
+++ D07oefeningen/D07achterstevoren/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace D07achterstevoren
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
