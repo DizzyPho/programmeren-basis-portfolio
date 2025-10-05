1cf1068c8ffedf0b94fe1dac435be8c037f05315
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-05 21:44:21 +0200

    Format multiples in string

--- D07oefeningen/D07veelvoudvan9/Program.cs
+++ D07oefeningen/D07veelvoudvan9/Program.cs
@@ -7,6 +7,8 @@
             for (int i = 1; i <= 10; i++)
             {
                 int multiple = i * 9;
+                string formatted = $"{i,2:d0} x 9 = {multiple,3:d0}";
+                Console.WriteLine(formatted);
             }
         }
     }
