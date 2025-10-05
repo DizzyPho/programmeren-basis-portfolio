ebd88a3c145ff1f9699f4f5e2ef901c267ff8b2c
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-05 22:35:03 +0200

    Check if inputted text has at least one punctuation mark

--- D07oefeningen/D07leestekens/Program.cs
+++ D07oefeningen/D07leestekens/Program.cs
@@ -1,9 +1,31 @@
-﻿namespace D07leestekens
+﻿using Helper.UserInputHelper;
+
+namespace D07leestekens
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            string text = UserInput.StringInput("Input some text: ");
+            bool foundPunct = false;
+
+            int lastIndex = text.Length - 1;
+            int index = 0;
+            do
+            {
+
+                if (Char.IsPunctuation(text[index]))
+                {
+                    Console.WriteLine("This text has at least one punctuation mark.");
+                    foundPunct = true;
+                }
+                index++;
+            } while (!foundPunct && index <= lastIndex);
+
+            if (!foundPunct) {
+                Console.WriteLine("This text contains no punctuation marks.");
+            }
+
         }
     }
 }
