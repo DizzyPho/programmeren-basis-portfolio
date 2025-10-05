0460f2f8ad4db155d32eae3724513a796c877767
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-05 22:17:06 +0200

    Set up project for new exercise

---
+++ D07oefeningen/D07karakterpositie/Program.cs
@@ -0,0 +1,17 @@
+﻿using Helper.UserInputHelper;
+
+namespace D07karakterpositie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = UserInput.StringInput("Input some text: ");
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                Console.WriteLine($"{i} = {text[i]}");
+            }
+        }
+    }
+}
