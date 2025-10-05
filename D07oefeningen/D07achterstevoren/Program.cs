62571559a8877f35ce3dd4aa51bd08099766d3cb
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-05 22:21:46 +0200

    Ask user to input string and display it backwards

--- D07oefeningen/D07achterstevoren/Program.cs
+++ D07oefeningen/D07achterstevoren/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace D07achterstevoren
+﻿using Helper.UserInputHelper;
+
+namespace D07achterstevoren
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string text = UserInput.StringInput("Input some text: ");
+            
+            int lastIndex = text.Length - 1;
+            string output = "";
+
+            for (int i = lastIndex; i >= 0; i--) { 
+                output += text[i];
+            }
+
+            Console.WriteLine(output);
         }
     }
 }
