c1cc098bec34ee18811b7168b563bfc0fdf4aa67
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-05 21:56:55 +0200

    Ask user to input radius and then calculate area and circumference

--- D07oefeningen/D07cirkel/Program.cs
+++ D07oefeningen/D07cirkel/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace D07cirkel
+﻿using Helper.UserInputHelper;
+
+namespace D07cirkel
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const double PI = Math.PI;
+            double radius = UserInput.DoubleInput("Input the circle radius: ");
+
+            double area = PI * Math.Pow(radius, 2);
+            double circumference = PI * radius * 2;
+
+
+            
         }
     }
 }
