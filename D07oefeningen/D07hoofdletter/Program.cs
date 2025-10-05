58f452f651d5bb6bc396dfcc0ca353dcdf9d8c77
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-05 22:40:29 +0200

    Check if text starts with lowercase or uppercase character

--- D07oefeningen/D07hoofdletter/Program.cs
+++ D07oefeningen/D07hoofdletter/Program.cs
@@ -8,6 +8,16 @@ namespace D07hoofdletter
         {
             string text = UserInput.StringInput("Input some text: ");
 
+            char firstLetter = text[0];
+
+            if (Char.IsUpper(firstLetter))
+            {
+                Console.WriteLine("This text starts with a capital letter.");
+
+            }
+            else {
+                Console.WriteLine("This text doesn't start with a capital letter");
+            }
         }
     }
 }
