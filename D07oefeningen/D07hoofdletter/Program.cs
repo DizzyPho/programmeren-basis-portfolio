d310bede8d070578a5637531601d02af69c0c244
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-05 22:36:32 +0200

    Set up project for new exercise

---
+++ D07oefeningen/D07hoofdletter/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace D07hoofdletter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
