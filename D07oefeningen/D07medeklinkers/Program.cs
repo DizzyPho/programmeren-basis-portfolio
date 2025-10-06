b7e3ddc2c623f0ff23612a11d73115aaffbe1759
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-06 08:44:53 +0200

    Count vowels and consonants in text

--- D07oefeningen/D07medeklinkers/Program.cs
+++ D07oefeningen/D07medeklinkers/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace D07medeklinkers
+﻿using Helper.UserInputHelper;
+
+namespace D07medeklinkers
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string text = UserInput.StringInput("Input some text: ");
+            int vowelCounter = 0;
+            int consCounter = 0;
+
+            foreach (char c in text) {
+                if (isVowel(c))
+                {
+                    vowelCounter++;
+                }
+                else if (isConsonant(c))
+                {
+                    consCounter++;
+                } 
+            }
+            Console.WriteLine($"This text contains {vowelCounter} vowels and {consCounter} consonants.");
         }
 
         static bool isVowel(char c)
