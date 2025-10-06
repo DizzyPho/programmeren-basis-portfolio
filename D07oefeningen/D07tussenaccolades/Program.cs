eb4354d3c48e2e0c2438355f554fcec85453c776
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-06 08:53:53 +0200

    Write methods to find first opening and closing accolade in text

--- D07oefeningen/D07tussenaccolades/Program.cs
+++ D07oefeningen/D07tussenaccolades/Program.cs
@@ -5,6 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            static int findOpenAccolade(string text) {
+                return text.IndexOf('{');            
+            }
+
+            static int findCloseAccolade(string text)
+            {
+                return text.IndexOf('}');
+            }
         }
     }
 }
