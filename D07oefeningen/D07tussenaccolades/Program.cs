44bdaf4688e6f77100421ae70355fba0ead1521e
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-06 08:49:03 +0200

    Set up project for new exercise

---
+++ D07oefeningen/D07tussenaccolades/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace D07tussenaccolades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
