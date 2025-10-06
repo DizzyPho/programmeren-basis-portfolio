32804f4cc81d2bd52198208ffc94959bc209f0c4
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-06 18:57:15 +0200

    Start from code given by assignment

--- D08oefeningen/D08gemeenten/Program.cs
+++ D08oefeningen/D08gemeenten/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] gemeenten = new string[8];
+
+            gemeenten[0] = "Brussel";
+            gemeenten[1] = "1000";
+            gemeenten[2] = "Antwerpen";
+            gemeenten[3] = "2000";
+            gemeenten[4] = "Brugge";
+            gemeenten[5] = "8000";
+            gemeenten[6] = "Gent";
+            gemeenten[7] = "9000";
         }
     }
 }
