89a3495b7f926776e0a600d80f90e685721a1da2
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-06 08:23:00 +0200

    Check how many e's are in given text

--- D07oefeningen/D07aantalkeere/Program.cs
+++ D07oefeningen/D07aantalkeere/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace D07aantalkeere
+﻿using Helper.UserInputHelper;
+
+namespace D07aantalkeere
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            string text = UserInput.StringInput("Give some text: ");
+            int eCounter = 0;
+            foreach (char c in text) {
+                if (isE(c)) { 
+                    eCounter++;
+                }
+            }
 
+            Console.WriteLine($"The character 'e' appears {eCounter} times in this text.");
         }
         static bool isE(char c)
         {
