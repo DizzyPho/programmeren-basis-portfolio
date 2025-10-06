4b869c9acd912d2b177dbb34e25fd6d2b50adb68
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-06 19:00:25 +0200

    Start from code given by assignment

--- D08oefeningen/D08dagentotaal/Program.cs
+++ D08oefeningen/D08dagentotaal/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] dagen = new int[12];
+
+            dagen[0] = 31;
+            dagen[1] = 28;
+            dagen[2] = 31;
+            dagen[3] = 30;
+            dagen[4] = 31;
+            dagen[5] = 30;
+            dagen[6] = 31;
+            dagen[7] = 31;
+            dagen[8] = 30;
+            dagen[9] = 31;
+            dagen[10] = 30;
+            dagen[11] = 31;
+
+            int totaal = 0;
         }
     }
 }
