6da35037f4b5e7d035726505aa0630e40fbee02f
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-06 19:17:52 +0200

    Format and print each item in array a

--- D08oefeningen/D08toongetallen/Program.cs
+++ D08oefeningen/D08toongetallen/Program.cs
@@ -5,6 +5,18 @@
         static void Main(string[] args)
         {
             int[] a = { 5, 3, 1, -1, -3 };
+            string output = string.Empty;
+            int length = a.Length;
+
+            for (int index = 0; index < length; index++) {
+                output += $"{a[index]}";
+
+                if (index != length - 1)  {
+                    output += ", ";
+                }
+            }
+
+            Console.WriteLine(output);
         }
     }
 }
