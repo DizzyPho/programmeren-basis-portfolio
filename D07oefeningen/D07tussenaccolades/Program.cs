b3185dfe5c6f0d61cb285e33e9dfc74b4c363c56
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-06 08:58:53 +0200

    Find open and close accolade index of given text

--- D07oefeningen/D07tussenaccolades/Program.cs
+++ D07oefeningen/D07tussenaccolades/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace D07tussenaccolades
+﻿using Helper.UserInputHelper;
+
+namespace D07tussenaccolades
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string text = UserInput.StringInput("Input some text: ");
+            int startIndex = findOpenAccolade(text);
+            int endIndex = findCloseAccolade(text);  
+
 
             static int findOpenAccolade(string text) {
                 return text.IndexOf('{');            
