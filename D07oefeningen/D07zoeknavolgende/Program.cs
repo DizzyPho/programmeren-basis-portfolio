ac5b0adc0700373ff019b353ecfe0ec02a486e1b
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-06 10:04:51 +0200

    Start from code of D07zoeken

--- D07oefeningen/D07zoeknavolgende/Program.cs
+++ D07oefeningen/D07zoeknavolgende/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace D07zoeknavolgende
+﻿using Helper.UserInputHelper;
+
+namespace D07zoeknavolgende
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string text = UserInput.StringInput("Input some text: ").ToLower();
+            string search = UserInput.StringInput("Give a piece of text to search for: ").ToLower();
+
+            int timesFound = 0;
+            int foundAtIndex = 0;
+
+            while (foundAtIndex != -1)
+            {
+                if (search == string.Empty)
+                {
+                    break;
+                }
+
+                foundAtIndex = text.IndexOf(search, foundAtIndex + 1);
+
+                if (foundAtIndex != -1)
+                {
+                    timesFound++;
+                }
+            }
+            Console.WriteLine($"Found your search term {timesFound} times.");
         }
     }
 }
