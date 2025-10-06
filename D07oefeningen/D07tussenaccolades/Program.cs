03cd7ade8c60f6c690006720909cf4372be83584
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-06 09:13:08 +0200

    Account for user not correctly inputing accolades

--- D07oefeningen/D07tussenaccolades/Program.cs
+++ D07oefeningen/D07tussenaccolades/Program.cs
@@ -7,19 +7,27 @@ namespace D07tussenaccolades
         static void Main(string[] args)
         {
             string text = UserInput.StringInput("Input some text: ");
-            int startIndex = findOpenAccolade(text) + 1; 
+            int startIndex = findOpenAccolade(text) + 1;
             int endIndex = findCloseAccolade(text);
 
             string output = "";
 
-            for (int index = startIndex; index < endIndex; index++) { 
-                output += text[index];
+            if (startIndex == 0 || endIndex == -1)
+            {
+                output = "Found nothing.";
+            } 
+            else
+            {
+                for (int index = startIndex; index < endIndex; index++)
+                {
+                    output += text[index];
+                }
             }
-
             Console.WriteLine(output);
 
-            static int findOpenAccolade(string text) {
-                return text.IndexOf('{');            
+            static int findOpenAccolade(string text)
+            {
+                return text.IndexOf('{');
             }
 
             static int findCloseAccolade(string text)
