e3d2703ef43cdc00b5a4ab85c8a03fece3fde264
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-06 08:36:47 +0200

    Define methods for checking if char is vowel and consonant

--- D07oefeningen/D07medeklinkers/Program.cs
+++ D07oefeningen/D07medeklinkers/Program.cs
@@ -6,5 +6,16 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        static bool isVowel(char c)
+        {
+            const string vowels = "aeiou";
+            return vowels.Contains(c);
+        }
+        static bool isConsonant(char c)
+        {
+            const string consonants = "bcdfghjklmnpqrstvwxyz";
+            return consonants.Contains(c);
+        }
     }
 }
