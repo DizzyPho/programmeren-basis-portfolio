caa4e05c048d9a78c368612b5baf7c0aa2aa1f46
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-07 10:18:32 +0200

    Set up project for next exercise

---
+++ D08oefeningen/D08woordslang/Program.cs
@@ -0,0 +1,22 @@
+﻿using Helper.UserInputHelper;
+
+namespace D08woordslang
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create array
+            //input 5 words via for loop
+            //put in array
+
+            //for each loop
+            //save last word to temp var
+            //break if first of current word != last of last word 
+            //else: add word to output string
+
+
+            
+        }
+    }
+}
