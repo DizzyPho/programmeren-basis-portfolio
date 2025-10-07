ccaa8398e5e0b900e3d7d604520813c1455bbb4f
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-07 20:04:15 +0200

    Set up project for new exercise

---
+++ D08oefeningen/D08posititiezoeken/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace D08posititiezoeken
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 5, 3, 1, -1, -3, 3, 9, -4 };
+
+        }
+    }
+}
