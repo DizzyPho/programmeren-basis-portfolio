38abfad9dd813f4783ebe1a8e4b5c375cd6b5400
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-07 20:15:46 +0200

    Find all indexes of given number in array a

--- D08oefeningen/D08posititiezoeken/Program.cs
+++ D08oefeningen/D08posititiezoeken/Program.cs
@@ -6,6 +6,34 @@
         {
             int[] a = { 5, 3, 1, -1, -3, 3, 9, -4 };
 
+            const int numberToFind = 8;
+
+            string indexes = string.Empty;
+            bool noInstancesLeft = false;
+
+            int previousInstance = -1;
+            while (!noInstancesLeft) { 
+                
+                
+                int foundAtIndex = Array.IndexOf(a, numberToFind, previousInstance + 1);
+
+                if (foundAtIndex == -1)
+                {
+                    noInstancesLeft = true;
+                }
+                else {
+                    indexes += foundAtIndex + " ";
+                }
+                previousInstance = foundAtIndex;
+            }
+
+            if (indexes == string.Empty) {
+                Console.WriteLine("No instances found.");
+            } else
+            {
+
+                Console.WriteLine($"Found {numberToFind} at index(es): {indexes}");
+            }
         }
     }
 }
