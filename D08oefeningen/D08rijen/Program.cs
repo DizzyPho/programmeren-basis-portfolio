b91f5e966a350f9a2bece96eab03885bbc6368d5
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-07 19:46:35 +0200

    Remove comments outling steps

--- D08oefeningen/D08rijen/Program.cs
+++ D08oefeningen/D08rijen/Program.cs
@@ -6,15 +6,7 @@ namespace D08rijen
     {
         static void Main(string[] args)
         {
-            //create array of length 6
-            //Ask user for input
-            //try to parse, if succeeds, add to 
-            //keep track of how many 
-            //loop 6 times
-            //break if empty thingy
 
-            // calc potential delta/factor based on first 2 items
-            // check if delta / factor applies to rest of array
             double[] row = GetRow();
 
             double potentialDelta = PotentialDelta(row);
