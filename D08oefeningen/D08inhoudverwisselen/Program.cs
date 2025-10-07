ed10b6eb980b0c4425dd3266a33b0bd2e69ef948
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-07 11:33:20 +0200

    Start from code provided by assignment

--- D08oefeningen/D08inhoudverwisselen/Program.cs
+++ D08oefeningen/D08inhoudverwisselen/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] a = { 12, 34, 56, 78, 90 };
+            int[] b = { 31, 42, 53, 64, 75 };
+
+            Console.Write("De inhoud van a voor de wissel : ");
+            Console.WriteLine(String.Join(',', a));
+
+            // SCHRIJF HIER JE CODE OM DE ARRAY INHOUDEN OM TE WISSELEN
+
+            Console.Write("De inhoud van a na de wissel : ");
+            Console.WriteLine(String.Join(',', a));
         }
     }
 }
