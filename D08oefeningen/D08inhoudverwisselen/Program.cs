08394b85aac9e0c6e3bc320947c34c5b49b89849
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-07 11:34:03 +0200

    Swap arrays a and b

--- D08oefeningen/D08inhoudverwisselen/Program.cs
+++ D08oefeningen/D08inhoudverwisselen/Program.cs
@@ -11,6 +11,9 @@
             Console.WriteLine(String.Join(',', a));
 
             // SCHRIJF HIER JE CODE OM DE ARRAY INHOUDEN OM TE WISSELEN
+            int[] temp = a;
+            a = b;
+            b = temp;
 
             Console.Write("De inhoud van a na de wissel : ");
             Console.WriteLine(String.Join(',', a));
