12835922609ceb4e7330223e96902d40e79a0697
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-07 15:06:57 +0200

    Outline program steps

--- D08oefeningen/D08rijen/Program.cs
+++ D08oefeningen/D08rijen/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //create array of length 6
+            //Ask user for input
+            //try to parse, if succeeds, add to 
+            //keep track of how many 
+            //loop 6 times
+            //break if empty thingy
+
+            // calc potential delta/factor based on first 2 items
+            // check if delta / factor applies to rest of array
+            
+
         }
     }
 }
