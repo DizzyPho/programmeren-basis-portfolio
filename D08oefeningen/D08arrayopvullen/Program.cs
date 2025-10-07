510dd6daca29ff37981698b8cc8315deeb695454
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-07 11:32:02 +0200

    Fill getallen with numbers 101-109

--- D08oefeningen/D08arrayopvullen/Program.cs
+++ D08oefeningen/D08arrayopvullen/Program.cs
@@ -7,7 +7,9 @@
             int[] getallen = new int[9];
 
             // opvullen
-
+            for (int i = 0; i < getallen.Length; i++) {
+                getallen[i] = i + 101;
+            }
 
             // afdrukken
             for (int index = 0; index < getallen.Length; index++)
