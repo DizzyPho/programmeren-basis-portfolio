7cde48a92913305c926e26dc9ab9abe3968d98cf
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-08 18:02:35 +0200

    Set up project for new exercise

---
+++ D09oefeningen/D09toonscore/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace D09toonscore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vakken = { "Frans", "Engels", "Wiskunde", "Duits", "L.O." };
+            int[] scores = { 34, 55, 20, 10, 80 };
+
+        }
+    }
+}
