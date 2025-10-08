dd800627e1991982cd4b85f3e2d8d9e322bc0f2f
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-08 18:15:49 +0200

    Look for input in vakken and give corresponding value from scores

--- D09oefeningen/D09toonscore/Program.cs
+++ D09oefeningen/D09toonscore/Program.cs
@@ -1,12 +1,26 @@
-﻿namespace D09toonscore
+﻿using Helper.UserInputHelper;
+
+namespace D09toonscore
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string[] vakken = { "Frans", "Engels", "Wiskunde", "Duits", "L.O." };
+            string[] vakken = { "frans", "engels", "wiskunde", "duits", "l.o." };
             int[] scores = { 34, 55, 20, 10, 80 };
 
+            string whichSubj = UserInput.StringInput("Van welk vak wil je je punten zien? ").ToLower();
+
+            int subject = Array.IndexOf(vakken, whichSubj);
+
+            if (subject == -1)
+            {
+                Console.WriteLine("Vak niet gevonden.");
+            }
+            else {
+                int score = scores[subject];
+                Console.WriteLine($"Je score voor {whichSubj} is {score}.");
+            }
         }
     }
 }
