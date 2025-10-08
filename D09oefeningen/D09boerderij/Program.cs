43a39905583827b3659cea4132aefcb7a99f61a9
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-08 17:17:15 +0200

    Find index of whichAnimal to see if it exists in array

--- D09oefeningen/D09boerderij/Program.cs
+++ D09oefeningen/D09boerderij/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace D09boerderij
+﻿using Helper.UserInputHelper;
+
+namespace D09boerderij
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             string[] boerderijDieren = { "kip", "koe", "paard", "geit", "schaap" };
+
+            string whichAnimal = UserInput.StringInput("Welke dier wil je opzoeken? ").ToLower();
+            int foundAnimal = Array.IndexOf(boerderijDieren, whichAnimal);
+
+            if (foundAnimal == -1)
+            {
+                Console.WriteLine("Dit is geen boerderijdier!");
+            }
+            else {
+                Console.WriteLine($"{whichAnimal} is een boerderijdier.");
+            }
         }
     }
 }
