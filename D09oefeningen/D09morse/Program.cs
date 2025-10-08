c669f4665668236fceb700d31633675428471814
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-08 18:28:26 +0200

    Take user input and break when user inputs /

--- D09oefeningen/D09morse/Program.cs
+++ D09oefeningen/D09morse/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace D09morse
+﻿using Helper.UserInputHelper;
+
+namespace D09morse
 {
     internal class Program
     {
@@ -7,6 +9,17 @@
             string[] morse = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
             char[] letters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
+            string input = string.Empty;
+            string decoded = string.Empty;
+
+            while (true) {
+
+                input = UserInput.StringInput("Input morse code for the next letter: ").Trim();
+
+                if (input == "/") {
+                    break;
+                }   
+            }
         }
     }
 }
