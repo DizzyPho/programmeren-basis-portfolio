adb129acbb83374ba89720567112fc38d1c2fa4a
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-08 17:07:29 +0200

    Paste array given by assignment

--- D09oefeningen/D09boerderij/Program.cs
+++ D09oefeningen/D09boerderij/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] boerderijDieren = { "kip", "koe", "paard", "geit", "schaap" };
         }
     }
 }
