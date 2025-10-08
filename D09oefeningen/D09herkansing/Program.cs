a0d80926ca7a3f54918e62c7acea043bcc1729e2
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-08 18:00:31 +0200

    Iterrate over array and break when finding a value < 10

--- D09oefeningen/D09herkansing/Program.cs
+++ D09oefeningen/D09herkansing/Program.cs
@@ -6,6 +6,14 @@
         {
             int[] puntenlijst = { 13, 16, 13, 18, 8, 12, 15, 3, 4, 11, 17, 18 };
 
+            bool herkansingNodig = false;
+            foreach (int punt in puntenlijst) {
+                if (punt < 10) { 
+                    herkansingNodig = true;
+                    break;
+                }
+            }
+            Console.WriteLine(herkansingNodig);
         }
     }
 }
