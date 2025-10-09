1baf0e8269967eedcde74d1bd499cb2d8b189248
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-09 22:47:22 +0200

    Check if two collection overlap

--- D10oefeningen/D10overlappend/Program.cs
+++ D10oefeningen/D10overlappend/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine(IsOverlapping(1, 3, 4, 13));
+        }
+
+        static bool IsOverlapping(int min1, int max1, int min2, int max2)
+        {
+            return (max1 >= min2) || (max2 >= min1);
         }
     }
 }
