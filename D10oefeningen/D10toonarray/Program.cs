c843bf314f279d35d1b0405c817a4c1c116c9039
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-09 22:33:59 +0200

    Print contents of array

--- D10oefeningen/D10toonarray/Program.cs
+++ D10oefeningen/D10toonarray/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] nums = { 4, 7, 9, 34, 2, 56, 34, 78 };
+            PrintArray(nums);
         }
+
+
+        static void PrintArray(int[] array) {
+            string arrayText = String.Join(", ", array);
+            Console.WriteLine(arrayText); 
+        }
+
+
     }
 }
