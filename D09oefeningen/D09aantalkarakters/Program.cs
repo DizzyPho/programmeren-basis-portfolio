1f7803e5a5b58e28bb4a15ed523ad1e935d7b633
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-09 09:08:43 +0200

    Start from code given by assignment

--- D09oefeningen/D09aantalkarakters/Program.cs
+++ D09oefeningen/D09aantalkarakters/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] woorden;
+
+            woorden = new string[] { "dit", "zijn", "een", "aantal", "woorden" }; // 3,7,10,16,23
+            //woorden = new string[] { "dit", "zijn", "dan", "weer", "wat", "andere", "woorden" }; // 3,7,10,14,17,23,30
+
+
+
+
+
+            //Console.WriteLine(string.Join(',', aantalKarakters));
         }
     }
 }
