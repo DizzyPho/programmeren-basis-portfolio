2fc216f14453830a36ddafff20f4914ceadd995a
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-09 22:39:35 +0200

    Calculate factorial of number n

--- D10oefeningen/D10faculteit/Program.cs
+++ D10oefeningen/D10faculteit/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace D10faculteit
+﻿using System.Net.WebSockets;
+
+namespace D10faculteit
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Factorial(5));
+        }
+
+        static int Factorial(int n) {
+
+            int result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+
+            return result;
         }
     }
 }
