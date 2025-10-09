3e21a1d5f35e19f4cdbd600285c934044dbce933
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-09 23:09:42 +0200

    Return number of days for any given month in a year

--- D10oefeningen/D10dagen/Program.cs
+++ D10oefeningen/D10dagen/Program.cs
@@ -7,11 +7,19 @@
 
             do
             {
+
+                Console.Write("Maand?: ");
+                int maand = int.Parse(Console.ReadLine());
                 Console.Write("Jaar?: ");
                 int jaar = int.Parse(Console.ReadLine());
-                int aantalDagen = DagenFebruari(jaar);
-                Console.WriteLine($"In februari van {jaar} zijn er {aantalDagen} dagen.");
+                string[] maanden = {"januari", "februari", "maart", "april", "mei", "juni", "juli",
+                        "augustus", "september", "oktober", "november", "december"};
+
+                int aantalDagen = DagenMaand(maand - 1, jaar);
+
+                Console.WriteLine($"In {maanden[maand - 1]} van {jaar} zijn er {aantalDagen} dagen.");
                 Console.WriteLine();
+
             } while (true);
 
 
@@ -24,6 +32,22 @@
             static bool IsSchrikkeljaar(int jaartal)
             {
                 return (jaartal % 400 == 0 || jaartal % 4 == 0 && jaartal % 100 != 0);
+            }
+
+            static int DagenMaand(int maand, int jaar)
+            {
+                if (maand == 1)
+                {
+                    return DagenFebruari(jaar);
+                }
+
+                int[] months31Days = [0, 2, 4, 6, 7, 9, 11];
+
+                if (months31Days.Contains(maand))
+                {
+                    return 31;
+                }
+                else { return 30; }
             }
         }
     }
