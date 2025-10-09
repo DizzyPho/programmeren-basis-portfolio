d3514ad4fcd335fb76643cceb73b2b235afea7cf
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-09 09:04:41 +0200

    Assemble array of combinations of each item in two arrays

--- D09oefeningen/D09speelkaarten/Program.cs
+++ D09oefeningen/D09speelkaarten/Program.cs
@@ -7,6 +7,19 @@
             string[] kleuren = { "harten", "klaver", "schoppen", "ruiten" };
             string[] waarden = { "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "tien", "landbouwer", "dame", "koning", "aas" };
 
+
+            string[] cards = new string[52];
+
+            int counter = 0;
+            foreach (string color in kleuren)
+            {
+                foreach (string value in waarden)
+                {
+                    cards[counter] = $"{color} {value}";
+                    counter++;
+                }
+            }
+
         }
     }
 }
