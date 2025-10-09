ff5e2b50ad43b3ddd94bc992899a82dc9cbe9972
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-09 08:40:47 +0200

    Set up project for new exercise

---
+++ D09oefeningen/D09temperatuur/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace D09temperatuur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
