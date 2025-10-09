1a06ed41dc58f52fe60ccbbf9c91f78e7adbb47a
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-09 22:36:48 +0200

    Set up project for new exercises

---
+++ D10oefeningen/D10faculteit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace D10faculteit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
