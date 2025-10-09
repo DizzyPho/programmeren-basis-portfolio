3c1f094fa31762af56360982d8f33595618af682
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-09 22:50:45 +0200

    Import code given by assignment

--- D10oefeningen/D10dagen/Program.cs
+++ D10oefeningen/D10dagen/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            static void Main()
+            {
+                do
+                {
+                    Console.Write("Jaar?: ");
+                    int jaar = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"In februari van {jaar} zijn er {...} dagen.");
+                    Console.WriteLine();
+                } while (true);
+            }
+
+
+
+static bool IsSchrikkeljaar(int jaartal)
+            {
+                return (jaartal % 400 == 0 || jaartal % 4 == 0 && jaartal % 100 != 0);
+            }
         }
     }
 }
