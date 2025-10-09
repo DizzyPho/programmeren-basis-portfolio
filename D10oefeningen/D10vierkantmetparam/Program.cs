d582b834320b10e0620b2bb9be5b323fab37a192
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-09 22:11:07 +0200

    Call method vierkant

--- D10oefeningen/D10vierkantmetparam/Program.cs
+++ D10oefeningen/D10vierkantmetparam/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Vierkant(7);
         }
 
         static void Vierkant(int lengte)
