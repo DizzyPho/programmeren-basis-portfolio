89c51b84bcd9f4d03c7f1d2d75cce09e9565861e
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-09 22:29:37 +0200

    Calculate sum between two numbers

--- D10oefeningen/D10toonsomtussen/Program.cs
+++ D10oefeningen/D10toonsomtussen/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(SumBetween(1,4));
+        }
+
+        static int SumBetween(int min, int max) {
+            int sum = 0;
+            for (int i = min; i <= max; i++) { 
+                sum += i;
+            }
+            return sum;
         }
     }
 }
