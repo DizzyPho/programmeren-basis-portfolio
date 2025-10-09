01dd543adcc7d0b575363d49810f4801a8c147d2
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-09 11:08:57 +0200

    Define list of swears

--- D09oefeningen/D09censuur/Program.cs
+++ D09oefeningen/D09censuur/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] swearWords = ["shit", "fuck", "javascript", "astarion", "waegenaar"];
+
+
         }
     }
 }
