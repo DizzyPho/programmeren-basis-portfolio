ce3a2f2d3667a35b2ed15ff838f79c9c80179626
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-09 10:47:17 +0200

    Ask user for input repeadedly

--- D09oefeningen/D09zoekhistoriek/Program.cs
+++ D09oefeningen/D09zoekhistoriek/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace D09zoekhistoriek
+﻿using Helper.UserInputHelper;
+
+namespace D09zoekhistoriek
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            while (true) {
+                string nextEntry = UserInput.StringInput("Give a next search term: ");
+            }
         }
     }
 }
