0a3bf4e83ba4d9f4d8cc58133a2f7220e7a1f6ee
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-09 09:42:55 +0200

    Start from code of D09speelkaarten

---
+++ D09oefeningen/D09durstenfeld/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace D09durstenfeld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static string[] SetKaarten() {
+            string[] kleuren = { "harten", "klaver", "schoppen", "ruiten" };
+            string[] waarden = { "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "tien", "landbouwer", "dame", "koning", "aas" };
+
+
+            string[] cards = new string[52];
+
+            int counter = 0;
+            foreach (string color in kleuren)
+            {
+                foreach (string value in waarden)
+                {
+                    cards[counter] = $"{color} {value}";
+                    counter++;
+                }
+            }
+
+            return cards;
+        }
+    }
+    }
+}
