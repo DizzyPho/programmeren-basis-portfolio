577d9fda96a3c8db3166a54e04c83060a3ccda6b
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-09 22:54:12 +0200

    Remove accidental nested main method

--- D10oefeningen/D10dagen/Program.cs
+++ D10oefeningen/D10dagen/Program.cs
@@ -4,20 +4,24 @@
     {
         static void Main(string[] args)
         {
-            static void Main()
+
+            do
             {
-                do
-                {
-                    Console.Write("Jaar?: ");
-                    int jaar = int.Parse(Console.ReadLine());
-                    Console.WriteLine($"In februari van {jaar} zijn er {...} dagen.");
-                    Console.WriteLine();
-                } while (true);
+                Console.Write("Jaar?: ");
+                int jaar = int.Parse(Console.ReadLine());
+                int aantalDagen = DagenFebruari(jaar);
+                Console.WriteLine($"In februari van {jaar} zijn er {aantalDagen} dagen.");
+                Console.WriteLine();
+            } while (true);
+
+
+            static int DagenFebruari(int jaar)
+            {
+                int dagen = IsSchrikkeljaar(jaar) ? 29 : 28;
+                return dagen;
             }
 
-
-
-static bool IsSchrikkeljaar(int jaartal)
+            static bool IsSchrikkeljaar(int jaartal)
             {
                 return (jaartal % 400 == 0 || jaartal % 4 == 0 && jaartal % 100 != 0);
             }
