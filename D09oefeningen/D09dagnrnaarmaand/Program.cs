13189a77b799af2a22273f9858a1bec3f5cf6721
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-09 08:38:25 +0200

    Check in which interval of aantalDagen dayNumber is

--- D09oefeningen/D09dagnrnaarmaand/Program.cs
+++ D09oefeningen/D09dagnrnaarmaand/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace D09dagnrnaarmaand
+﻿using Helper.UserInputHelper;
+
+namespace D09dagnrnaarmaand
 {
     internal class Program
     {
@@ -7,7 +9,20 @@
             int[] aantalDagen = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
             string[] maandNamen = { "Januari", "Februari", "Maart", "April", "Mei", "Juni", "Juli", "Augustus", "September", "Oktober", "November", "December" };
 
+            int dayNumber = UserInput.IntegerInput("Geef een dagnummer: ");
 
+            int totalDays = 0;
+            for (int index = 0; index < aantalDagen.Length; index++) {
+
+                totalDays += aantalDagen[index];
+
+                if (dayNumber <= totalDays) {
+                    string month = maandNamen[index];
+                    Console.WriteLine($"De maand is {month}.");
+                    break;
+                }
+
+            }
         }
     }
 }
