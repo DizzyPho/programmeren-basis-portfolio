82ded52e4f56723f8d9d52ba8400e5be1b9389eb
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-09 14:04:03 +0200

    Create list of names

--- D09oefeningen/D09namenalfabetisch/Program.cs
+++ D09oefeningen/D09namenalfabetisch/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> names = new List<string>();
+
         }
     }
 }
