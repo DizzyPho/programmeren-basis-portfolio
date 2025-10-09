2dcb59341ade2dea5cdaa540e48d4986a2245cd6
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-09 14:20:21 +0200

    Put 10 user inputted numbers into list

--- D09oefeningen/D09vijfkleinste/Program.cs
+++ D09oefeningen/D09vijfkleinste/Program.cs
@@ -1,13 +1,20 @@
-﻿namespace D09vijfkleinste
+﻿using Helper.UserInputHelper;
+
+namespace D09vijfkleinste
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             List<int> numbers = new List<int>();
-            
 
-            
+            while (numbers.Count < 10) {
+
+                int newNumber = UserInput.IntegerInput("Enter a number");
+                numbers.Add(newNumber);                    
+            }
+
+ 
         }
     }
 }
