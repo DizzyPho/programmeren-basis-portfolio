daf1962362937a69155d73b13914df0c58ec2920
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-09 11:06:25 +0200

    Set up project for next exercise

---
+++ D09oefeningen/D09censuur/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace D09censuur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
