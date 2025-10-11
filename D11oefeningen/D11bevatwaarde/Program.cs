e5a5485d638120c24a0c3f9fe1008a8d70f80261
dizzypho <184835166+DizzyPho@users.noreply.github.com.>
2025-10-11 17:22:51 +0200

    Ask user to input five numbers

--- D11oefeningen/D11bevatwaarde/Program.cs
+++ D11oefeningen/D11bevatwaarde/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace D11
+﻿using Helper.UserInputHelper;
+
+namespace D11
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            int validNumbers = 0;
+            int[] uniqueNumbers = new int[5];
 
+            while (validNumbers < 5) {
+
+                int currentNumber = validNumbers + 1;
+                int userNumber = UserInput.IntegerInput($"Give number #{currentNumber}: ");
+
+                validNumbers++;
+
+            }
         }
 
         static bool Contains(int value, int[] numbers) {
