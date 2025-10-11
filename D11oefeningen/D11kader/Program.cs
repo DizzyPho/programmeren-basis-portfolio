f1d4aa6addd2a8a2cfe54369ac177f63e43ecf91
dizzypho <184835166+DizzyPho@users.noreply.github.com.>
2025-10-11 12:03:42 +0200

    Display text inside red border

--- D11oefeningen/D11kader/Program.cs
+++ D11oefeningen/D11kader/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace D11kader
+﻿using Helper.UserInputHelper;
+
+namespace D11kader
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string textToDisplay = UserInput.StringInput("What text do you wanna see in a border? ");
+            DisplayInBorder(textToDisplay);
+        }
+
+        static void DisplayInBorder(string output)
+        {
+
+            int len = output.Length;
+            string fullLine = new string('*', len + 4);
+            string textLine = $"* {output} *";
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(fullLine);
+            Console.WriteLine(textLine);
+            Console.WriteLine(fullLine);
         }
     }
 }
