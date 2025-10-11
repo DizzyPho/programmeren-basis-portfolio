7d2cab48175a9799c145829c1064ea8c7da380af
dizzypho <184835166+DizzyPho@users.noreply.github.com.>
2025-10-11 12:26:49 +0200

    Count amount of specified characters in a string

--- D11oefeningen/D11charcount/Program.cs
+++ D11oefeningen/D11charcount/Program.cs
@@ -5,8 +5,30 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine(GetCharCount("Hello World",'H')); 
         }
 
-        
+        static int GetCharCount(string str, char c)
+        {
+            int charCount = 0;
+            int foundAtIndexPrevious = -1;
+            while (true)
+            {
+
+                int foundAtIndex = str.IndexOf(c, foundAtIndexPrevious + 1);
+                if (foundAtIndex == -1)
+                {
+                    break;
+                }
+                else
+                {
+                    charCount += 1;
+                }
+                foundAtIndexPrevious = foundAtIndex;
+            }
+            return charCount;
+        }
+
+
     }
 }
