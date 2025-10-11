fd6737d6eb60f8fb00c0138da554246340570b92
dizzypho <184835166+DizzyPho@users.noreply.github.com.>
2025-10-11 11:51:59 +0200

    Convert score out of 100 to letter code

--- D11oefeningen/D11letterscore/Program.cs
+++ D11oefeningen/D11letterscore/Program.cs
@@ -6,5 +6,21 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        static char GetLetterCode(int percentage) {
+
+            char letter = percentage switch
+            {
+                > 82 => 'A',
+                > 67 => 'B',
+                > 52 => 'C',
+                > 37 => 'D',
+                > 22 => 'E',
+                _ => 'F'
+            };
+
+            return letter;
+
+        }
     }
 }
