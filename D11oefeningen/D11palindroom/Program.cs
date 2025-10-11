2304b6ff6234edc3b3baa9aa5c786412d3b172bd
dizzypho <184835166+DizzyPho@users.noreply.github.com.>
2025-10-11 12:34:49 +0200

    Reverse given string

--- D11oefeningen/D11palindroom/Program.cs
+++ D11oefeningen/D11palindroom/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(ReverseText("Hello world"));
+
+        }
+
+        static string ReverseText(string text)
+        {
+
+            string reversed = string.Empty;
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                reversed += text[i];
+            }
+            return reversed;
         }
     }
 }
