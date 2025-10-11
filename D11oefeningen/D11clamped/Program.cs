77f1e5df6a37079aada77eb918d16e7ff4a9b884
dizzypho <184835166+DizzyPho@users.noreply.github.com.>
2025-10-11 12:43:45 +0200

    Clamp number between min and max value

--- D11oefeningen/D11clamped/Program.cs
+++ D11oefeningen/D11clamped/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Clamp(1, 11, 10));
+        }
+
+        static int Clamp(int min, int number, int max) {
+
+            int clampedNum;
+
+            if (number < min)
+            {
+                clampedNum = min;
+            }
+            else if (number > max)
+            {
+                clampedNum = max;
+            }
+            else
+            {
+                clampedNum = number;
+            }   
+            return clampedNum;
         }
     }
 }
